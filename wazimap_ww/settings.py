37f581e979258cc9f67da52ff751ea7579a44459
LOUKOPKOU <louwhyman@gmail.com>
2018-02-05 13:51:59 +0200

    fixed geo and versions

--- wazimap_ww/settings.py
+++ wazimap_ww/settings.py
@@ -16,7 +16,8 @@ WAZIMAP['levels'] = {
         'children': ['country']
     },
     'country': {
-        'plural': 'countries'
+        'plural': 'countries',
+        'children': []
     }
 }
 
@@ -24,59 +25,103 @@ WAZIMAP['comparative_levels'] = ['world', 'country']
 
 WAZIMAP['geometry_data'] = {
   '': {
-      'country': 'geo/world.geojson', 
-      'vdc': 'geo/country.geojson'
+      'world': 'geo/world.geojson', 
+      'country': 'geo/country.geojson'
   }
 }
 
+
+WAZIMAP['default_geo_version'] = ''
 WAZIMAP['profile_builder'] = 'wazimap_ww.profiles.get_profile'
 WAZIMAP['map_centre'] = [0, 0]
 WAZIMAP['map_zoom'] = 2
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#       'standard': {
+#         'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+#       }
+#     },
+#     'filters':{
+
+#     },
+#     'handlers': {
+#       'default': {
+#         'level': 'DEBUG',
+#         'class': 'logging.handlers.RotatingFileHandler',
+#         'filename': 'logs/mylog.log',
+#         'maxBytes': 1024*1024*5, # 5 MB,
+#         'backupCount': 5,
+#         'formatter': 'standard',
+#       },
+#       'request_handler': {
+#         'level': 'DEBUG',
+#         'class': 'logging.handlers.RotatingFileHandler',
+#         'filename': 'logs/django_request.log',
+#         'maxBytes': 1024*1024*5, # 5 MB,
+#         'backupCount': 5,
+#         'formatter': 'standard',
+#       }
+#     },
+#     'loggers': {
+#       '': {
+#         'handlers': ['default'],
+#         'level': 'DEBUG',
+#         'propagate': True
+#       },
+#       'django.request': {
+#         'handlers': ['request_handler'],
+#         'level': 'DEBUG',
+#         'propagate': False
+#       },
+#       'gdal.request': {
+#         'handlers': ['request_handler'],
+#         'level': 'DEBUG',
+#         'propagate': False
+#       }
+#     }
+# }
+
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
     'formatters': {
-      'standard': {
-        'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-      }
-    },
-    'filters':{
-
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(process)d %(thread)d %(message)s'
+        },
     },
     'handlers': {
-      'default': {
-        'level': 'DEBUG',
-        'class': 'logging.handlers.RotatingFileHandler',
-        'filename': 'logs/mylog.log',
-        'maxBytes': 1024*1024*5, # 5 MB,
-        'backupCount': 5,
-        'formatter': 'standard',
-      },
-      'request_handler': {
-        'level': 'DEBUG',
-        'class': 'logging.handlers.RotatingFileHandler',
-        'filename': 'logs/django_request.log',
-        'maxBytes': 1024*1024*5, # 5 MB,
-        'backupCount': 5,
-        'formatter': 'standard',
-      }
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
     },
     'loggers': {
-      '': {
-        'handlers': ['default'],
-        'level': 'DEBUG',
-        'propagate': True
-      },
-      'django.request': {
-        'handlers': ['request_handler'],
-        'level': 'DEBUG',
-        'propagate': False
-      },
-      'gdal.request': {
-        'handlers': ['request_handler'],
-        'level': 'DEBUG',
-        'propagate': False
-      }
+        '': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+        'census': {
+            'level': 'DEBUG' if DEBUG else 'INFO',
+        },
+        'django': {
+            'level': 'DEBUG' if DEBUG else 'INFO',
+        },
+        'django.template': {
+            'level': 'ERROR',
+        },
+        'wazimap': {
+            'level': 'ERROR',
+        },
+        'gdal': {
+            'level': 'ERROR',
+        },
+        'shapely': {
+            'level': 'ERROR',
+        },
     }
 }
