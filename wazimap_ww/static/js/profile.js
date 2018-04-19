2e3e1394a6fa5cac0df121a7703112b72553dd90
LOUKOPKOU <louwhyman@gmail.com>
2018-04-19 15:41:09 +0200

    Last changes

---
+++ wazimap_ww/static/js/profile.js
@@ -0,0 +1,12 @@
+
+$('a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+
+  var target = this.hash;
+  var $target = $(target);
+
+  if ($target.length) {
+    window.scroll(0, $target.offset().top - 90);
+    if ('pushState' in window.history) window.history.pushState({}, null, target);
+  }
+});
